35760e7872daa59f4b1a46788f1923d70c611c51
Enrique Chenu <enchenu@gmail.com>
2017-01-30 21:57:49 -0300

    Primera Subida

---
+++ src/app/layouts/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Angular2TokenService } from 'angular2-token';
+ 
+ 
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+ 
+  constructor(private _tokenService: Angular2TokenService) { }
+ 
+  ngOnInit() {
+  }
+ 
+  public toggled(open: boolean): void {}
+ 
+  public collapsed(event: any): void {}
+ 
+  public expanded(event: any): void {}
+ 
+  signOut()
+  {
+    this._tokenService.signOut().subscribe(
+      res =>      console.log(res),
+      error =>    console.log(error)
+    );
+  }
+}
